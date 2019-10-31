d1922dd8480d4f84da28cf2c92f934c2b0b29647
BrandonKates <bjk224@cornell.edu>
2019-10-31 11:04:11 -0400

    update script

--- generate_euclidean_data.sh
+++ generate_euclidean_data.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
-python generate_graphs.py --num_graphs 10000 --num_nodes 10 --filename MST10_10000_Euclidean.txt
-python generate_graphs.py --num_graphs 10000 --num_nodes 20 --filename MST20_10000_Euclidean.txt
-python generate_graphs.py --num_graphs 10000 --num_nodes 30 --filename MST30_10000_Euclidean.txt
-python generate_graphs.py --num_graphs 10000 --num_nodes 40 --filename MST40_10000_Euclidean.txt
-python generate_graphs.py --num_graphs 10000 --num_nodes 50 --filename MST50_10000_Euclidean.txt
-python generate_graphs.py --num_graphs 10000 --num_nodes 60 --filename MST60_10000_Euclidean.txt
-python generate_graphs.py --num_graphs 10000 --num_nodes 70 --filename MST70_10000_Euclidean.txt
-python generate_graphs.py --num_graphs 10000 --num_nodes 80 --filename MST80_10000_Euclidean.txt
-python generate_graphs.py --num_graphs 10000 --num_nodes 90 --filename MST90_10000_Euclidean.txt
-python generate_graphs.py --num_graphs 10000 --num_nodes 100 --filename MST100_10000_Euclidean.txt
+mkdir ./data/
+python generate_graphs.py --num_graphs 10000 --num_nodes 10 --filename ./data/MST10_10000_Euclidean.txt
+python generate_graphs.py --num_graphs 10000 --num_nodes 20 --filename ./data/MST20_10000_Euclidean.txt
+python generate_graphs.py --num_graphs 10000 --num_nodes 30 --filename ./data/MST30_10000_Euclidean.txt
+python generate_graphs.py --num_graphs 10000 --num_nodes 40 --filename ./data/MST40_10000_Euclidean.txt
+python generate_graphs.py --num_graphs 10000 --num_nodes 50 --filename ./data/MST50_10000_Euclidean.txt
+python generate_graphs.py --num_graphs 10000 --num_nodes 60 --filename ./data/MST60_10000_Euclidean.txt
+python generate_graphs.py --num_graphs 10000 --num_nodes 70 --filename ./data/MST70_10000_Euclidean.txt
+python generate_graphs.py --num_graphs 10000 --num_nodes 80 --filename ./data/MST80_10000_Euclidean.txt
+python generate_graphs.py --num_graphs 10000 --num_nodes 90 --filename ./data/MST90_10000_Euclidean.txt
+python generate_graphs.py --num_graphs 10000 --num_nodes 100 --filename ./data/MST100_10000_Euclidean.txt
